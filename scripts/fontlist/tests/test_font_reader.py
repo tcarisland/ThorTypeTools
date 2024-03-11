a9dcf46ef711859554a10ef23ec5d99388a65c06
tcarisland <tcarisland@gmail.com>
2024-03-11 22:48:04 +0100

    added dotenv with template closes #6

--- scripts/fontlist/tests/test_font_reader.py
+++ scripts/fontlist/tests/test_font_reader.py
@@ -1,8 +1,14 @@
 # tests/test_font_reader.py
 from src.fontlist.font_reader import read_font_file
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env file
+load_dotenv()
+THORTYPE_PATH = os.getenv("THORTYPE_PATH")
+# Access environment variables
 def test_read_font_file():
-    path = "../../../../thortype/public/static/fonts/belleview.otf"
+    path = str(THORTYPE_PATH) + "/public/static/fonts/belleview.otf"
     font = read_font_file(path)
     assert font is not None
     # Add more assertions as needed
