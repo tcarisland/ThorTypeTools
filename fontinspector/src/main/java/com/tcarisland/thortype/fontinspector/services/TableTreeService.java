0809c642963bc0da754849bf45ac33a56e25ea76
tcarisland <tcarisland@gmail.com>
2024-03-13 16:57:39 +0100

    extending table tree #8

--- fontinspector/src/main/java/com/tcarisland/thortype/fontinspector/services/TableTreeService.java
+++ fontinspector/src/main/java/com/tcarisland/thortype/fontinspector/services/TableTreeService.java
@@ -1,15 +1,20 @@
 package com.tcarisland.thortype.fontinspector.services;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.fontbox.ttf.TTFTable;
 import org.springframework.stereotype.Service;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Service
 @Data
+@Slf4j
 public class TableTreeService {
     public JTree createJTreeFromHashMap(String name, Map<String, TTFTable> hashMap) {
         DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(name);
@@ -18,8 +23,29 @@ public class TableTreeService {
             TTFTable child = entry.getValue();
             DefaultMutableTreeNode parentNode = new DefaultMutableTreeNode(parent);
             rootNode.add(parentNode);
-            parentNode.add(new DefaultMutableTreeNode(child.getTag()));
+            parentNode.add(createDataFromTable(child));
         }
         return new JTree(rootNode);
+    }
+
+    public DefaultMutableTreeNode createDataFromTable(TTFTable table) {
+        DefaultMutableTreeNode tableRootNode = new DefaultMutableTreeNode(table.getTag());
+        for(Field field : table.getClass().getDeclaredFields()) {
+            try {
+                field.setAccessible(true);
+                String parent = field.getName();
+                DefaultMutableTreeNode parentNode = new DefaultMutableTreeNode(parent);
+                String fieldData = "" + field.get(table);
+                tableRootNode.add(parentNode);
+                Object child = field.get(table);
+                log.info("child class {} {} {}", field.getName(), field.getType(), child != null ? child.getClass() : null);
+                log.info("child is primitive {} {}", field.getType().isPrimitive(), field.getClass());
+                DefaultMutableTreeNode childNode = new DefaultMutableTreeNode("" + child);
+                parentNode.add(childNode);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tableRootNode;
     }
 }
