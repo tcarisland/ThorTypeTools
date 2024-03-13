dbcbd77fcc9ad563977d06aed546579af71366d4
tcarisland <tcarisland@gmail.com>
2024-03-13 17:15:24 +0100

    setting FlatDarculaLeaf for mac closes #16

--- fontinspector/src/main/java/com/tcarisland/thortype/fontinspector/gui/FontInspectorGUI.java
+++ fontinspector/src/main/java/com/tcarisland/thortype/fontinspector/gui/FontInspectorGUI.java
@@ -1,5 +1,7 @@
 package com.tcarisland.thortype.fontinspector.gui;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatLightLaf;
 import com.tcarisland.thortype.fontinspector.FontInspectorEventListener;
 import com.tcarisland.thortype.fontinspector.FontInspectorProperties;
 import com.tcarisland.thortype.fontinspector.services.TableTreeService;
@@ -20,6 +22,7 @@ import java.util.Map;
 public class FontInspectorGUI {
 
     private final TableTreeService tableTreeService;
+    private final FontInspectorProperties properties;
     private JFrame fontInspectorFrame;
     private JPanel mainContentPane;
     private FontTableList tableListPane;
@@ -29,29 +32,45 @@ public class FontInspectorGUI {
                             @Autowired FontInspectorEventListener eventListener,
                             @Autowired TableTreeService tableTreeService) {
         this.tableTreeService = tableTreeService;
+        this.properties = properties;
         EventQueue.invokeLater(() -> {
-            FontInspectorFrame frame = new FontInspectorFrame(properties);
-            frame.setJMenuBar(menuFactory.initMenu(frame.getRootPane()));
-            eventListener.setFontInspectorGUI(this);
-            this.mainContentPane = initContentPane();
-            this.tableListPane = initTableListPane();
-            this.mainContentPane.add(this.tableListPane.getTableListPanel(), BorderLayout.WEST);
-            frame.setContentPane(this.mainContentPane);
-            this.fontInspectorFrame = frame;
             try {
+                FontInspectorFrame frame;
                 if(isWindows()) {
-                    frame = (FontInspectorFrame) this.setWindowsLookAndFeel(frame);
+                    frame = this.initFrameWithWindowsLookAndFeel();
+                } else if(isMac()) {
+                    frame = this.initFrameWithMacLookAndFeel();
+                } else {
+                    frame = new FontInspectorFrame(properties);
                 }
+                frame.setJMenuBar(menuFactory.initMenu(frame.getRootPane()));
+                eventListener.setFontInspectorGUI(this);
+                this.mainContentPane = initContentPane();
+                this.tableListPane = initTableListPane();
+                this.mainContentPane.add(this.tableListPane.getTableListPanel(), BorderLayout.WEST);
+                frame.setContentPane(this.mainContentPane);
+                this.fontInspectorFrame = frame;
+                frame.repaint();
+                frame.setVisible(true);
             } catch (Exception e) {
                 log.error("could not set look and feel", e);
             }
-            frame.repaint();
-            frame.setVisible(true);
         });
     }
 
-    public FontInspectorFrame setWindowsLookAndFeel(FontInspectorFrame frame) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    private FontInspectorFrame initFrameWithMacLookAndFeel() throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+        FlatDarculaLaf.setup();
+        System.setProperty("com.apple.mrj.application.apple.menu.about.name", "FontInspector");
+        UIManager.setLookAndFeel( new FlatDarculaLaf());
+        FontInspectorFrame frame = new FontInspectorFrame(this.properties);
+        SwingUtilities.updateComponentTreeUI(frame);
+        return frame;
+    }
+
+    public FontInspectorFrame initFrameWithWindowsLookAndFeel() throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        FontInspectorFrame frame = new FontInspectorFrame(this.properties);
         SwingUtilities.updateComponentTreeUI(frame);
         return frame;
     }
@@ -62,6 +81,14 @@ public class FontInspectorGUI {
         log.info("os.name {}", osName);
         log.info("is windows {}", isWindows);
         return isWindows;
+    }
+
+    public boolean isMac() {
+        String osName = System.getProperty(("os.name"));
+        boolean isMac = osName.toLowerCase().contains("mac os x");
+        log.info("os.name {}", osName);
+        log.info("is mac {}", isMac);
+        return isMac;
     }
 
     public JPanel initContentPane() {
